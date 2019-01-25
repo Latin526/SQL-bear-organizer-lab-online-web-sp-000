9358e5027b210c9be88b3e4f40e2e6fdf33b18e2
Yasmin Torres <yasmintorres90@hotmail.com>
2019-01-25 23:42:50 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,3 +1,3 @@
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Mr. Chocolate', 15, 'Male', 'Black', 'Mello', 0);
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('ROWDY', 6, 'Male', 'Brown', 'unpredictable', 1);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Tabitha', 8, 'Female', 'White', )
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Tabitha', 8, 'Female', 'White', '')
