7f33906af2fab61632a3088fb84830483b44dd0a
Yasmin Torres <yasmintorres90@hotmail.com>
2019-01-26 00:00:05 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,7 +1,7 @@
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Mr. Chocolate', 20, 'M', 'dark brown', 'calm', 0);
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('ROWDY', 10, 'M', 'black', 'intense', 1);
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Tabitha', 6, 'F', 'dark brown', 'nice', 1);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Sergeant Brown', 10, 'Male', 'Black', 'Aggresive', 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Sergeant Brown', 19, 'M', 'Black', 'Aggresive', 1);
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Melissa', 5, 'Female', 'White', 'Tamed', 1);
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Grinch', 17, 'Male', 'Brown', 'Aggresive', 0);
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Wendy', 11, 'Female', 'Grey', 'Mellow', 0);
